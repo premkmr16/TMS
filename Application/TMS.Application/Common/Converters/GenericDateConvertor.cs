c8ab304791e0d105760f092d79861ef74c2f6fd6
Prem <premkumarmbtechit@gmail.com>
2025-05-14 23:06:34 +0530

    xml comments for date convertor

--- Application/TMS.Application/Common/Converters/GenericDateConvertor.cs
+++ Application/TMS.Application/Common/Converters/GenericDateConvertor.cs
@@ -4,10 +4,25 @@ using System.Text.Json.Serialization;
 
 namespace TMS.Application.Common.Converters;
 
+/// <summary>
+/// 
+/// </summary>
+/// <typeparam name="T"></typeparam>
 public class GenericDateConvertor<T> : JsonConverter<T>
 {
+    /// <summary>
+    /// 
+    /// </summary>
     private const string DateFormat = "dd-MM-yyyy";
 
+    /// <summary>
+    /// 
+    /// </summary>
+    /// <param name="reader"></param>
+    /// <param name="typeToConvert"></param>
+    /// <param name="options"></param>
+    /// <returns></returns>
+    /// <exception cref="NotSupportedException"></exception>
     public override T Read(ref Utf8JsonReader reader, Type typeToConvert, JsonSerializerOptions options)
     {
         var str = reader.GetString();
@@ -20,7 +35,7 @@ public class GenericDateConvertor<T> : JsonConverter<T>
         }
         else
         {
-            if (typeof(T) == typeof(DateTime))  return (T)(object)DateTime.ParseExact(str, DateFormat, CultureInfo.InvariantCulture);
+            if (typeof(T) == typeof(DateTime)) return (T)(object)DateTime.ParseExact(str, DateFormat, CultureInfo.InvariantCulture);
            
             if (typeof(T) == typeof(DateTimeOffset)) return (T)(object)DateTimeOffset.ParseExact(str!, DateFormat, CultureInfo.CurrentCulture);
         }
@@ -28,6 +43,13 @@ public class GenericDateConvertor<T> : JsonConverter<T>
         throw new NotSupportedException();
     }
 
+    /// <summary>
+    /// 
+    /// </summary>
+    /// <param name="writer"></param>
+    /// <param name="value"></param>
+    /// <param name="options"></param>
+    /// <exception cref="NotSupportedException"></exception>
     public override void Write(Utf8JsonWriter writer, T value, JsonSerializerOptions options)
     {
         if (value is DateTime dateTime) writer.WriteStringValue(dateTime == DateTime.MinValue ? "" : dateTime.ToString(DateFormat));
